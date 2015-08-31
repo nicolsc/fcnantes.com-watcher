b155030d2bc80b65e5e1fdcfdbc95cbdae995303
nicolsc <nicolas.lesconnec@gmail.com>
2015-08-31 14:38:59 +0200

    Last-minute variable renaming ... uncomplete :/

--- fcnantes.sh
+++ fcnantes.sh
@@ -3,10 +3,10 @@
 SUFFIX="<"
 MONITOR_FROM=11628 #latest : http://fcnantes.com/articles/article2809.php?num=11627
 HOW_MANY=20
-SCAN_UNTIL=$((LATEST_NEWS+HOW_MANY))
+SCAN_UNTIL=$((MONITOR_FROM+HOW_MANY))
 while true
 do
-  for NUM in $(seq $LATEST_NEWS 1 $SCAN_UNTIL); do
+  for NUM in $(seq $MONITOR_FROM 1 $SCAN_UNTIL); do
    TITLE=`curl -s http://fcnantes.com/articles/article2809.php\?num\=$NUM | grep "</title>"`
    TITLE=${TITLE%<*}
    echo "$NUM\t$TITLE"
